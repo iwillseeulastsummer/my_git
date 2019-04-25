7d19d479a25f4077d236a08991beb365392d58a7
iwillseeulastsummer <49877742+iwillseeulastsummer@users.noreply.github.com>
2019-04-25 19:21:41 +0300

    Add files via upload

---
+++ Homework2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework2
+{
+    class Rectangle
+    {
+        static void Main(string[] args)
+        {
+            byte a = 0;
+            byte b = 0;
+            bool repeat = true;
+            while (repeat)
+            {
+                do
+                {
+                    Console.Write("Enter Length = ");
+                }
+                while (!byte.TryParse(Console.ReadLine(), out a));
+                do
+                {
+                    Console.Write("Enter Width = ");
+                }
+                while (!byte.TryParse(Console.ReadLine(), out b));
+                if (a <= 0 || b <= 0)
+                {
+                    Console.WriteLine("Error, try again? Y/N");
+                    var ki = Console.ReadKey(true);
+                    if (ki.Key != ConsoleKey.Y)
+                        repeat = false;
+                    continue;
+                }
+                Console.WriteLine("Length: {0}, Width: {1}, Square: {2}", a, b, a * b);
+                repeat = false;
+                Console.ReadKey();
+            }
+        }
+    }
+}
